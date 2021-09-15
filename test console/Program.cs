def2f842cbb325748f8829a4bf6d3fed57fd0a12
Emil <emil377g@edu.easj.dk>
2021-09-15 17:08:16 +0200

    Add project files.

---
+++ test console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FootBallLib;
+
+namespace test_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FootballPlayer p = new FootballPlayer(5, "eeeeeee", 52525252, 52);
+            Console.WriteLine(p);
+        }
+    }
+}
