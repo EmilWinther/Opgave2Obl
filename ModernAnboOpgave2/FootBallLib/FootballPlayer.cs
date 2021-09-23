87f69b8bd7cea0cec4c23e33805e025838f40d3a
Emil Winther <emil377g@edu.easj.dk>
2021-09-23 19:50:45 +0200

    Add files via upload

---
+++ ModernAnboOpgave2/FootBallLib/FootballPlayer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace FootBallLib
+{
+    /// <summary>
+    /// Contains properties for a footballplayer with exceptions and constraints.
+    /// </summary>
+    public class FootballPlayer
+    {
+        private string _name;
+        private int _id;
+        private double _price;
+        private int _shirtNumber;
+
+        public FootballPlayer(int id, string name, double price, int shirtnumber)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtnumber; 
+        }
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) throw new ArgumentException("Name length should be more than 4 characters");
+                _name = value;
+            }
+        }
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("price", value, "price under 0");
+                _price = value;
+            }
+
+        }
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (1 <= value && value <= 100) _shirtNumber = value;
+                else throw new ArgumentOutOfRangeException("shirtnumber", value, "Shirtnumber not between 1-100");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + "\nName: " + Name + "\nPrice: " + Price + "\nShirtnumber: " + ShirtNumber;
+        }
+    }
+}
---
+++ ModernAnboOpgave2/FootBallLibTests1/FootballPlayerTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FootBallLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootBallLib.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer _footballPlayer;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _footballPlayer = new FootballPlayer(1, "Messi", 5000000.23, 30);
+        }
+
+
+        [TestMethod()]
+        public void ConstructorTest()
+        {
+            FootballPlayer fb = new FootballPlayer(1, "Messe", 9235252.523232, 52);
+
+            Assert.AreEqual(1, fb.Id);
+            Assert.AreEqual("Messe", fb.Name);
+            Assert.AreEqual(9235252.523232, fb.Price);
+            Assert.AreEqual(52, fb.ShirtNumber);
+        }
+
+
+        [TestMethod()]
+        public void NameTest()
+        {
+            _footballPlayer.Name = "Messi";
+            Assert.AreEqual("Messi", _footballPlayer.Name);
+            Assert.ThrowsException<ArgumentException>(() => _footballPlayer.Name = "M");
+        }
+
+
+        [TestMethod()]
+        public void TestPrice()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.Price = -52);
+        }
+
+
+        [TestMethod()]
+        public void TestShirtNumber()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.ShirtNumber = -30);
+            _footballPlayer.ShirtNumber = 1;
+            Assert.AreEqual(1, _footballPlayer.ShirtNumber);
+            _footballPlayer.ShirtNumber = 100;
+            Assert.AreEqual(100, _footballPlayer.ShirtNumber);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.ShirtNumber = 120);
+        }
+    }
+}
